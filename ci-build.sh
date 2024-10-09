acafaa4d693a56ffc8f73f0a48f59e671957acc1
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-10-08 22:54:43 -0400

    fixes

--- ci-build.sh
+++ ci-build.sh
@@ -5,8 +5,9 @@ mkdir Prusa-Firmware-Buddy
 git clone https://github.com/prusa3d/Prusa-Firmware-Buddy.git Prusa-Firmware-Buddy
 
 scripts/build.sh $1 mk4
-    # Upload the built file in the background
-curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" --upload-file "build/mk4_release_boot/firmware.bbf" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/${$1}/${mk4_firmware.bbf}"
+sudo pip3 install pipenv
+# Upload the built file in the background
+curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" --upload-file "build/mk4_release_boot/firmware.bbf" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/$1/mk4_firmware.bbf"
 
 # Wait for all background jobs to complete
 echo "Build and upload process completed."
