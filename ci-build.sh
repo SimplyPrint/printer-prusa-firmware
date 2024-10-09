db6f19bea3b545a53f4244ac7fb2bcf06d1b021f
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-10-09 15:35:26 -0400

    updated many scripts

--- ci-build.sh
+++ ci-build.sh
@@ -19,6 +19,31 @@ git clone https://github.com/prusa3d/Prusa-Firmware-Buddy.git Prusa-Firmware-Bud
 uv pip install -r Prusa-Firmware-Buddy/requirements.txt
 
 scripts/build.sh $1 mk4
+
+PACKAGE_URL="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/${output_name}/$1"
+while true; do
+    # Get the package details (handling single and array responses)
+    # shellcheck disable=SC2154
+    PACKAGE_INFO=$(curl --header "PRIVATE-TOKEN: ${package_registry_delete_token}" --silent "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages?package_name=${output_name}&package_version=$1")
+
+    # Check if the package exists (handling object vs array response)
+    PACKAGE_ID=$(echo "$PACKAGE_INFO" | jq -r 'if type=="array" and length > 0 then .[0].id else null end')
+
+    if [ -n "$PACKAGE_ID" ] && [ "$PACKAGE_ID" != "null" ]; then
+      echo "Deleting existing package version: ${VERSION} (Package ID: ${PACKAGE_ID})"
+      # Attempt to delete the package
+      curl --request DELETE --header "PRIVATE-TOKEN: $package_registry_delete_token" --silent "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/${PACKAGE_ID}"
+
+      # Sleep for a short time to avoid overloading the server
+      sleep 2
+    else
+      # Exit the loop if no package exists
+      echo "Package version ${VERSION} has been successfully deleted or does not exist."
+      break
+    fi
+  done
+
+
 # Upload the built file in the background
 curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" --upload-file "build/mk4_release_boot/firmware.bbf" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/$1/$1/mk4_firmware.bbf"
 
--- make_patches.sh
+++ make_patches.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 repo_path=Prusa-Firmware-Buddy-DEV ./generate_patches.sh
+
+
+# Check if smartcopy exists
+if ! command -v smartcopy &> /dev/null
+then
+    echo "smartcopy not found, running the download script."
+    curl -L --silent -o update_smartcopy.sh "https://gitlab.rylanswebsite.com/rylan-meilutis/smartcopy/-/raw/master/update_smartcopy.sh?ref_type=heads" && chmod +x update_smartcopy.sh && ./update_smartcopy.sh && rm update_smartcopy.sh
+
+else
+    echo "smartcopy found."
+fi
+
+smartcopy -o ./new_patches/* ./patches/v6.1.0
