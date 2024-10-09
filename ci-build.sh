96d626edb166d926d1c2117ee49738d277e1271d
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-10-08 23:18:32 -0400

    python3.12

--- ci-build.sh
+++ ci-build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-apt update && apt install -y python3 python3-pip python3-venv git curl wget
-python3 -m venv venv
+apt update && apt install -y python3 python3-pip python3-venv git curl wget software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt update
+apt install python3.12
+apt install python3.12-distutils
+
+python3.12 -m venv venv
 
 pip3 install pipenv
 
