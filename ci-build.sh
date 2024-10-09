0ad96dd979c2cb62c7143ef5dbb3fbb973d5c79b
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-10-08 22:55:19 -0400

    fix the order

--- ci-build.sh
+++ ci-build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+sudo pip3 install pipenv
+
 rm -rf Prusa-Firmware-Buddy
 
 mkdir Prusa-Firmware-Buddy
 git clone https://github.com/prusa3d/Prusa-Firmware-Buddy.git Prusa-Firmware-Buddy
 
 scripts/build.sh $1 mk4
-sudo pip3 install pipenv
 # Upload the built file in the background
 curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" --upload-file "build/mk4_release_boot/firmware.bbf" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/$1/mk4_firmware.bbf"
 
