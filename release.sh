41836d20c555cc4a7ed8775c90a40131beadae89
Rylan Meilutis <rylan.meilutis@gmail.com>
2025-01-28 21:18:55 -0500

    screen lock is wip, fixed the bug where the firmware crashed when using ssl

--- release.sh
+++ release.sh
@@ -2,10 +2,10 @@
 
 # Ensure submodules are initialized
 
-rm -rf Prusa-Firmware-Buddy
+#rm -rf Prusa-Firmware-Buddy
 
-mkdir Prusa-Firmware-Buddy
-git clone https://github.com/prusa3d/Prusa-Firmware-Buddy.git Prusa-Firmware-Buddy
+#mkdir Prusa-Firmware-Buddy
+#git clone https://github.com/prusa3d/Prusa-Firmware-Buddy.git Prusa-Firmware-Buddy
 
 
 #scripts/build.sh v5.0.0 mk4,xl
@@ -14,4 +14,6 @@ git clone https://github.com/prusa3d/Prusa-Firmware-Buddy.git Prusa-Firmware-Bud
 #scripts/build.sh v5.0.0 mk4,xl
 #scripts/build.sh v5.1.0-alpha1 mini
 #scripts/build.sh v6.1.0 mk4
-scripts/build.sh v6.1.4 mk4
+./make_patches.sh v6.1.4 && ./local_build.sh v6.1.4
+
+
